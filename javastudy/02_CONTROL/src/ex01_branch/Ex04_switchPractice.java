6625b8c3a14b5980c6c0e96e397fc4ae0d079002
jhw3603 <jhw3603@naver.com>
2022-07-20 17:32:45 +0900

    2022-07-20

---
+++ javastudy/02_CONTROL/src/ex01_branch/Ex04_switchPractice.java
@@ -0,0 +1,51 @@
+package ex01_branch;
+
+public class Ex04_switchPractice {
+
+	public static void main(String[] args) {
+		
+		// 연습
+		// 분기 출력하기
+		// 1~3월   : 1분기
+		// 4~6월   : 2분기
+		// 7~9월   : 3분기
+		// 10~12월 : 4분기
+		
+		int month = 4;
+		
+		switch(month / 3) {
+		
+		}
+				
+				
+		// 연습
+		// 점수에 따른 학점
+		// score       grade
+		// 100~90		A
+		// 89~80		B
+		// 79~70		C
+		// 69~60		D
+		// 59~0			F
+
+		int score = 100;
+		String grade;
+				
+		
+		// System.out.println(score + "점은 " + grade + "학점입니다.");
+		
+		// 연습
+		// 등급(1,2,3)에 따른 권한 출력
+		// 1등급 : 쓰기 실행 읽기
+		// 2등급 : 실행 읽기
+		// 3등급 : 읽기
+		// 나머지 : 없음
+		int level = 4;
+		String right = ""; // 권한
+
+		
+		System.out.println(right);
+		
+		
+	}
+
+}

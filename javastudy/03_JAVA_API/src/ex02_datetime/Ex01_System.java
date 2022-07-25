007baee9515ea328aa1f71fb27e3ee8bc943b91d
jhw3603 <jhw3603@naver.com>
2022-07-25 17:36:19 +0900

    2022-07-25

--- javastudy/03_JAVA_API/src/ex02_datetime/Ex01_System.java
+++ javastudy/03_JAVA_API/src/ex02_datetime/Ex01_System.java
@@ -6,6 +6,7 @@ public class Ex01_System {
 		
 		// 1. 타임스탬프(timestamp) <===================================  파일 이름으로 사용하기도 함.
 		// 1970-01-01 0:00부터 1/1000초마다 증가하는 long 타입의 정수값
+		// 중복을 예방하는 수단으로 활용되기도 함. (수치가 수시로 변화하기 때문에)
 		long timestamp = System.currentTimeMillis();
 		System.out.println(timestamp);
 		

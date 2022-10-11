3896437e7f14398477675bf5c6e168820103d221
jhw3603 <jhw3603@naver.com>
2022-10-11 17:49:30 +0900

    20221011

---
+++ jspstudy/01_Servlet/src/main/java/ex06/MovieJSONServlet.java
@@ -0,0 +1,111 @@
+package ex06;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/MovieJSONServlet")
+public class MovieJSONServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String clientId = "4H16u9bbdy10BBd8HKjl";
+		String clientSecret = "FSJHue2eMD";
+		
+		request.setCharacterEncoding("UTF-8");
+		String query = request.getParameter("query");
+		String display = request.getParameter("display");
+		
+		try {
+			query = URLEncoder.encode(query, "UTF-8");
+		} catch(UnsupportedEncodingException e) {
+			response.setContentType("text/plain; charset=UTF-8");
+			PrintWriter out = response.getWriter();
+			out.println("검색어 인코딩 실패");
+			out.close();
+		}
+		
+		String apiURL = "https://openapi.naver.com/v1/search/movie.json?query=" + query + "&display=" + display;
+		URL url = null;
+		HttpURLConnection con = null;
+		try {
+			url = new URL(apiURL);
+			con = (HttpURLConnection)url.openConnection();
+		} catch(MalformedURLException e) {
+			response.setContentType("text/plain; charset=UTF-8");
+			PrintWriter out = response.getWriter();
+			out.println("API URL이 잘못되었습니다.");
+			out.close();
+		} catch(IOException e) {
+			response.setContentType("text/plain; charset=UTF-8");
+			PrintWriter out = response.getWriter();
+			out.println("API 연결이 실패했습니다.");
+			out.close();
+		}
+		
+		try {
+			con.setRequestMethod("GET");
+			con.setRequestProperty("X-Naver-Client-Id", clientId);
+			con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
+		} catch(IOException e) {
+			response.setContentType("text/plain; charset=UTF-8");
+			PrintWriter out = response.getWriter();
+			out.println("API 요청이 실패했습니다.");
+			out.close();
+		}
+		
+		BufferedReader reader = null;
+		try {
+			int responseCode = con.getResponseCode();
+			if(responseCode == HttpURLConnection.HTTP_OK) {
+				reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			} else {
+				reader = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+			}
+		} catch(IOException e) {
+			response.setContentType("text/plain; charset=UTF-8");
+			PrintWriter out = response.getWriter();
+			out.println("API 응답 스트림 생성이 실패했습니다.");
+			out.close();
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		String line = null;
+		try {
+			while((line = reader.readLine()) != null) {
+				sb.append(line);
+			}
+			
+		} catch(IOException e) {
+			response.setContentType("text/plain; charset=UTF-8");
+			PrintWriter out = response.getWriter();
+			out.println("API 응답이 실패했습니다.");
+			out.close();
+		}
+		
+		response.setContentType("application/json; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println(sb.toString());
+		out.close();
+		
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}

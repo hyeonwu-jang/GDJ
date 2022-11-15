5c796f4453543bc5056591278c62ba7e11e5b9ad
jhw3603 <jhw3603@naver.com>
2022-11-15 17:51:11 +0900

    20221115

--- springstudy/13_Pjt_User/src/main/java/com/gdu/app13/mapper/UserMapper.java
+++ springstudy/13_Pjt_User/src/main/java/com/gdu/app13/mapper/UserMapper.java
@@ -1,5 +1,7 @@
 package com.gdu.app13.mapper;
 
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.gdu.app13.domain.RetireUserDTO;
@@ -8,7 +10,20 @@ import com.gdu.app13.domain.UserDTO;
 @Mapper
 public interface UserMapper {
 	
-	public UserDTO selectUserById(String id);
+	public UserDTO selectUserByMap(Map<String, Object> map);
+	
 	public RetireUserDTO selectRetireUserById(String id);
-	public UserDTO selectUserByEmail(String email);
+	
+	public int insertUser(UserDTO user);
+	
+	public int updateAccessLog(String id);
+	public int insertAccessLog(String id);
+	
+	// 회원탈퇴 (user 테이블 탈퇴, retire 테이블 추가)
+	public int deleteUser(int userNo);
+	public int insertRetireUser(RetireUserDTO retireUser);
+	
+	
+	
+	
 }

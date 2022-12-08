5770b2dba5a927b082620d92ad6c174e168dfe17
jhw3603 <jhw3603@naver.com>
2022-12-08 20:58:45 +0900

    20221208

--- springstudy/REST/src/main/java/com/gdu/rest/controller/MemberRestController.java
+++ springstudy/REST/src/main/java/com/gdu/rest/controller/MemberRestController.java
@@ -1,11 +1,16 @@
 package com.gdu.rest.controller;
 
 import java.util.Map;
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +28,10 @@ import com.gdu.rest.service.MemberService;
 	5. CRUD 처리 방식
 			       URL             Method
 		1) 삽입   /members		   POST
- 
+ 		2) 목록   /members         GET
+ 		3) 상세   /members/1       GET
+ 		4) 수정   /members         PUT
+ 		5) 삭제   /members/1       DELETE
  */
 
 
@@ -38,6 +46,32 @@ public class MemberRestController {
 	@PostMapping(value="/members", produces="application/json")
 	public Map<String, Object> addMember(@RequestBody MemberDTO member, HttpServletResponse response) {
 		return memberService.register(member, response);
+	}
+	
+	// 목록
+	@GetMapping(value="/members/page/{page}", produces="application/json")
+	public Map<String, Object> getMemberList(@PathVariable(value="page", required=false) Optional<String> opt) {
+		int page = Integer.parseInt(opt.orElse("1"));
+		return memberService.getMemberList(page);
+	}
+	
+	// 상세
+	@GetMapping(value="/members/{memberNo}", produces="application/json")
+	public Map<String, Object> getMember(@PathVariable(value="memberNo", required=false) Optional<String> opt) {
+		int memberNo = Integer.parseInt(opt.orElse("0"));
+		return memberService.getMemberByNo(memberNo);
+	}
+	
+	// 수정
+	@PutMapping(value="/members", produces="application/json")
+	public Map<String, Object> modifyMember(@RequestBody Map<String, Object> map, HttpServletResponse response) {
+		return memberService.modifyMember(map, response);
+	}
+	
+	// 삭제
+	@DeleteMapping(value="/members/{memberNoList}", produces="application/json")
+	public Map<String, Object> deleteMemberList(@PathVariable String memberNoList) {
+		return memberService.removeMemberList(memberNoList);
 	}
 	
 }
